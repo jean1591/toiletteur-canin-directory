3987687b16b78ad498a7fb922b33bda786e8f7c9
Jean Robertou <j-rob@live.fr>
2024-10-11 07:50:13 +0200

    chore: delete useless pages and routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,7 @@ import './globals.css'
 
 import { Inter } from 'next/font/google'
 import { Metadata } from 'next'
-import NextAuthProvider from './components/NextAuthProvider'
 import Script from 'next/script'
-import { StoreProvider } from '@/store/StoreProvider'
-import { Toaster } from 'react-hot-toast'
 import { classNames } from '@/utils/classNames'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -56,22 +53,17 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <StoreProvider>
-      <html lang="en" className="scroll-smooth">
-        <body
-          className={classNames(inter.className, 'bg-stone-100 text-slate-800')}
-        >
-          <NextAuthProvider>
-            <Script
-              defer
-              src="https://analytics.rb2.fr/script.js"
-              data-website-id="d1a73c24-62d2-4171-859f-da080ee2209b"
-            />
-            <Toaster position="bottom-right" reverseOrder={true} />
-            {children}
-          </NextAuthProvider>
-        </body>
-      </html>
-    </StoreProvider>
+    <html lang="en" className="scroll-smooth">
+      <body
+        className={classNames(inter.className, 'bg-stone-100 text-slate-800')}
+      >
+        <Script
+          defer
+          src="https://analytics.rb2.fr/script.js"
+          data-website-id="d1a73c24-62d2-4171-859f-da080ee2209b"
+        />
+        {children}
+      </body>
+    </html>
   )
 }
